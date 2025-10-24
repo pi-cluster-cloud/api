c4e0cbcb20c7c9203f0951a5bd9a02182fdd5646
Julian Palmisano <julp4lm@gmail.com>
2025-10-24 10:38:22 -0400

    201 tests user controller

--- __tests__/user.controller.test.ts
+++ __tests__/user.controller.test.ts
@@ -2,10 +2,31 @@ import prisma from '../src/utils/prisma';
 import * as userController from '../src/controller/user.controller';
 import { Role } from '@prisma/client';
 import * as userSchema from '../src/schema/user.schema';
-import { omit } from 'lodash';
+import { has, omit } from 'lodash';
+import { hashPassword } from '../src/service/user.service';
+//import { hashPassword } from '../src/service/user.service';
 
-describe('USER CONTROLLER', () => {
+
+
+describe('USER CONTROLLER', () => {\
+    // 201 Tests
     describe('FUNCTION createUserHandler', () => {
         it('should return 201 and created user without password', async () => {
-            
+            const login = {
+                body: {
+                    email: 'test@test.com'
+                    , password: hashPassword
+                    , firstName: 'Test'
+                    , lastName: 'User'
+                    , role: Role.user
+                }};
+                //expect (await userController.createUserHandler(String, hashPassword)).toBeDefined();
         }
+
+
+
+
+// `201` Tests with the newly created user object on success
+// `409` Tests if a unique constraint (phoneNumber, email) fails
+// `400` Tests for other Prisma client errors
+// `500` Tests for unknown errors
